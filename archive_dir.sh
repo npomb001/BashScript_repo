3ac713a312abfa5747b99ebbd085bc58694afa9e
NutPom <npomb001@ucr.edu>
2018-11-07 11:34:27 -0800

    add archive_dir.sh

---
+++ archive_dir.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --mem 128G
+#SBATCH --job-name="archive"
+#SBATCH --time=0-02:00:00
+
+
+###This script is for compressing large directory for archive.
+
+cd $SLURM_SUBMIT_DIR
+
+###Directory name for compressing
+DIR=BacQ2FWMojaveLE
+
+###If the archive file doesn't exist, then it will start "tar"
+if [ ! -f $DIR.archive.tar.gz ]; then
+ tar -zcvf $DIR.archive.tar.gz $DIR/
+else 
+ echo Archive file is already generated.
+fi
